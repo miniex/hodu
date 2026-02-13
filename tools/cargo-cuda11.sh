f00d1b231f88d854c95f7eb8099945c84e589c67
Han Damin <miniex@daminstudio.net>
2026-02-14 02:34:00 +0900

    chore: remove cuda11 docker/script and rename check-lib.sh to check.sh

--- tools/cargo-cuda11.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-    -v "$(pwd):/app" \
-    -v "$HOME/.cargo/registry:/root/.cargo/registry" \
-    -v "$HOME/.cargo/git:/root/.cargo/git" \
-    -e RUSTC_WRAPPER="" \
-    -e SCCACHE_DIR="" \
-    hodu-cuda11 cargo "$@"
